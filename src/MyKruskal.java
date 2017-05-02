d7ddca399ca862f01aaa65a27448ed4bcfacdecd
Hobo Noir <hobonetics@gmail.com>
2017-05-01 23:41:51 -0400

    outlined kruskal

--- src/MyKruskal.java
+++ src/MyKruskal.java
@@ -19,23 +19,52 @@ package src;
 import support.graph.*;
 import net.datastructures.*;
 import java.util.Iterator;
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class MyKruskal<V> implements MinSpanForest<V> {
 
-    /** 
-     * This method implements Kruskal's algorithm and extends it slightly
-     * to account for disconnected graphs.  You must return the collection
-     * of edges of the Minimum Spanning Forest (MSF) for the given graph, g.
-     * 
-     * This must run in O(|E|log|V|) time.
-     * @param g Your graph (created in MyGraph)
-     * @param visualizer Only used if you implement the optional animation.
-     * @return returns a data structure that contains the edges of your MSF that implements java.util.Collection
-     */
-    @Override
-    public Collection<CS16Edge<V>> genMinSpanForest(AdjacencyMatrixGraph<V> g, CS16GraphVisualizer<V> visualizer) {
-        return null;
-    }
+	AdjacencyMatrixGraph<V> g;
+	CS16GraphVisualizer<V> visualizer;
 
+	/**
+	 * This method implements Kruskal's algorithm and extends it slightly to
+	 * account for disconnected graphs. You must return the collection of edges
+	 * of the Minimum Spanning Forest (MSF) for the given graph, g.
+	 * 
+	 * This must run in O(|E|log|V|) time.
+	 * 
+	 * @param g
+	 *            Your graph (created in MyGraph)
+	 * @param visualizer
+	 *            Only used if you implement the optional animation.
+	 * @return returns a data structure that contains the edges of your MSF that
+	 *         implements java.util.Collection
+	 */
+	@Override
+	public Collection<CS16Edge<V>> genMinSpanForest(AdjacencyMatrixGraph<V> g, CS16GraphVisualizer<V> visualizer) {
+		this.g = g;
+		this.visualizer = visualizer;
+
+		// put each edge into a adaptable heap priority queue
+
+		// put each vertex in it's own cloud (use decorator)
+
+		// process each vertex in the queue
+		return processEdges();
+	}
+
+	private Collection<CS16Edge<V>> processEdges() {
+		Collection<CS16Edge<V>> edges = new ArrayList<CS16Edge<V>>();
+
+		// pop min edge
+
+		// get opposite vertices
+
+		// if vertices are in different clouds, unite them and add vertex to
+		// return variable
+		// TODO -- need unionFind algo
+
+		return edges;
+	}
 }
